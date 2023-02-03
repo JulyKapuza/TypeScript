2f1ea815769ed4b3917de6463814fca1ad4c5fdf
Juliia Kapuza <kapuzajuliia@gmail.com>
2023-02-03 17:46:00 +0200

    compiler options

---
+++ dist/button.js
@@ -0,0 +1,8 @@
+"use strict";
+const buttonOnClick = document.querySelector('button');
+if (buttonOnClick) {
+    buttonOnClick.addEventListener('click', () => {
+        console.log('Click');
+    });
+}
+//# sourceMappingURL=button.js.map
---
+++ src/button.ts
@@ -0,0 +1,7 @@
+const buttonOnClick = document.querySelector('button')!;
+
+if (buttonOnClick) {
+    buttonOnClick.addEventListener('click', () => {
+    console.log('Click');
+  });
+}
